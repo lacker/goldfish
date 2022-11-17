15bf4ec434cd2ac70479f6d245a83826b5d165e0
Kevin Lacker <lacker@gmail.com>
2022-11-17 14:49:07 -0800

    a little bit of rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+enum Card {
+    Coin,
+    Dancer,
+    Foxy,
+    Pillager,
+    Potion,
+    Scabbs,
+    Shark,
+}
+
+fn cost(card: &Card) -> i32 {
+    match card {
+        Card::Coin => 0,
+        Card::Dancer => 2,
+        Card::Foxy => 2,
+        Card::Pillager => 6,
+        Card::Potion => 4,
+        Card::Scabbs => 4,
+        Card::Shark => 4,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("cost of dancer: {}", cost(&Card::Dancer));
 }
