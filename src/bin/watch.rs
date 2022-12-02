acebedd28c0d4dc9dbc152f049039fb5b6738c44
Kevin Lacker <lacker@gmail.com>
2022-12-02 14:24:53 -0800

    fix a bunch of parsing problems

--- src/bin/watch.rs
+++ src/bin/watch.rs
@@ -41,22 +41,29 @@ fn read_log(last_create_game_line: usize) -> Result<LogData, std::io::Error> {
     // This happens when we shadowstep, and perhaps other times.
     let mut cost_map: BTreeMap<i32, i32> = BTreeMap::new();
 
+    // Figure out which player id we are. -1 means unknown.
+    // We can also use this regex to find out where the game starts.
+    let mut our_player_id = -1;
+    let player_id_re = Regex::new(r"^.*PlayerID=(\d+), PlayerName=lacker#1660.*$").unwrap();
+
     let card_id_re = Regex::new(r"^.*Updating Entity.* id=(\d+) .* CardID=(\w+).*$").unwrap();
-    let damage_re = Regex::new(r"^.*cardId=HERO_.*player=2.*tag=DAMAGE value=(\d+).*$").unwrap();
-    let armor_re = Regex::new(r"^.*cardId=HERO_.*player=2.*tag=ARMOR value=(\d+).*$").unwrap();
+    let damage_re =
+        Regex::new(r"^.*cardId=HERO_.*player=(\d+).*tag=DAMAGE value=(\d+).*$").unwrap();
+    let armor_re = Regex::new(r"^.*cardId=HERO_.*player=(\d+).*tag=ARMOR value=(\d+).*$").unwrap();
     let cost_re =
-        Regex::new(r"^.*TAG_CHANGE.*id=(\d+).*player=1.*tag=COST value=(\d+).*$").unwrap();
+        Regex::new(r"^.*TAG_CHANGE.*id=(\d+).*player=(\d+).*tag=COST value=(\d+).*$").unwrap();
 
     let skip_n = log_data.last_create_game_line;
     let enum_lines = || lines.iter().enumerate().skip(skip_n);
 
     for (i, line) in enum_lines() {
-        if line.contains("CREATE_GAME") {
+        if let Some(captures) = player_id_re.captures(line) {
             log_data.opponent_damage = 0;
             log_data.opponent_armor = 0;
             card_id_map.clear();
             cost_map.clear();
             log_data.last_create_game_line = i;
+            our_player_id = captures[1].parse().unwrap();
         }
         if let Some(captures) = card_id_re.captures(line) {
             let id = captures[1].parse::<i32>().unwrap();
@@ -64,14 +71,32 @@ fn read_log(last_create_game_line: usize) -> Result<LogData, std::io::Error> {
             card_id_map.insert(id, card_id.to_string());
         }
         if let Some(captures) = damage_re.captures(line) {
-            log_data.opponent_damage = captures[1].parse::<i32>().unwrap();
+            // println!("damage line: {}", line);
+            let player_id = captures[1].parse::<i32>().unwrap();
+            if our_player_id < 0 {
+                println!("warning: unknown player id but saw damage line: {}", line);
+            } else if player_id != our_player_id {
+                log_data.opponent_damage = captures[2].parse::<i32>().unwrap();
+            }
         }
         if let Some(captures) = armor_re.captures(line) {
+            let player_id = captures[1].parse::<i32>().unwrap();
+            if our_player_id < 0 {
+                println!("warning: unknown player id but saw armor line");
+            } else if player_id != our_player_id {
+                log_data.opponent_armor = captures[2].parse::<i32>().unwrap();
+            }
             log_data.opponent_armor = captures[1].parse::<i32>().unwrap();
         }
         if let Some(captures) = cost_re.captures(line) {
+            let player_id = captures[1].parse::<i32>().unwrap();
+            if our_player_id < 0 {
+                println!("warning: unknown player id but saw cost line");
+            } else if player_id != our_player_id {
+                continue;
+            }
             let id = captures[1].parse::<i32>().unwrap();
-            let cost = captures[2].parse::<i32>().unwrap();
+            let cost = captures[3].parse::<i32>().unwrap();
             cost_map.insert(id, cost);
         }
     }
@@ -96,7 +121,7 @@ fn read_log(last_create_game_line: usize) -> Result<LogData, std::io::Error> {
     let mut seen_ids: BTreeSet<i32> = BTreeSet::new();
     let known_card_re =
         Regex::new(r"^.*entityName=([^=]+) id=(\d+) zone=(?:HAND|SETASIDE).*$").unwrap();
-    let unknown_card_re = Regex::new(r"^.*option.*type=POWER.* id=(\d+).*player=1.*$").unwrap();
+    let unknown_card_re = Regex::new(r"^.*option.*type=POWER.* id=(\d+).*player=(\d)+.*$").unwrap();
 
     let mut handle_card = |id: i32, card: Card| {
         if seen_ids.contains(&id) {
@@ -125,12 +150,15 @@ fn read_log(last_create_game_line: usize) -> Result<LogData, std::io::Error> {
         } else if unknown_card_re.is_match(line) {
             let caps = unknown_card_re.captures(line).unwrap();
             let id = caps[1].parse::<i32>().unwrap();
+            let player_id = caps[2].parse::<i32>().unwrap();
+            if player_id != our_player_id {
+                continue;
+            }
             if let Some(card_id) = card_id_map.get(&id) {
                 let c = Card::from_card_id(card_id);
-                if c == Card::Unknown {
-                    println!("unknown id: {} card id: {}", id, card_id);
+                if c != Card::Unknown {
+                    handle_card(id, c);
                 }
-                handle_card(id, c);
             }
         }
     }
